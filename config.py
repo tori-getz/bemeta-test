0180cfe1254de2e5f2f1b29fe95781690a888b37
getzzZ <tori@Mac-Pro-getzzZ.local>
2021-02-04 20:31:31 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+
+# Config file
+
+# PostgreSQL
+DATABASE_NAME = ""
+DATABASE_USER = ""
+DATABASE_PASSWORD = ""
+DATABASE_HOST = ""
+
+# Airtable
+AIRTABLE_BASE_KEY = ""
+AIRTABLE_TABLE_NAME + ""
+
+
+#
+# Лучше конечно все эти данные хранить в системных переменных.
+# Но с python я пока только знакомлюсь, но оставлю пока так. 
+# В случае чего переделать недолго. =)
+#
---
+++ db.py
@@ -0,0 +1,5 @@
+
+from loguru import logger
+
+def database_connect():
+    logger.debug("Подключение к базе данных...")
